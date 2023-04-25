dd76e4cf396baf8674581f6057582bd4f3846b62
Amit kumar <92637182+notgetin18@users.noreply.github.com>
2023-04-25 12:57:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -107,34 +107,34 @@ const formatCurr = function (value, locale, currency) {
   }).format(value);
 };
 
-// const displayMovements = function (acc, sort = false) {
-//   containerMovements.innerHTML = '';
+const displayMovements = function (acc, sort = false) {
+  containerMovements.innerHTML = '';
 
-//   const movs = sort
-//     ? acc.movements.slice().sort((a, b) => a - b)
-//     : acc.movements;
+  const movs = sort
+    ? acc.movements.slice().sort((a, b) => a - b)
+    : acc.movements;
 
-//   movs.forEach(function (mov, i) {
-//     const type = mov > 0 ? 'deposit' : 'withdrawal';
+  movs.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
 
-//     const date = new Date(acc.movementsDates[i]);
-//     const displayDate = formatMovementDate(date, acc.locale);
+    const date = new Date(acc.movementsDates[i]);
+    const displayDate = formatMovementDate(date, acc.locale);
 
-//     const formattedMov = formatCurr(mov, acc.locale, acc.currency);
+    const formattedMov = formatCurr(mov, acc.locale, acc.currency);
 
-//     const html = `
-//       <div class="movements__row">
-//         <div class="movements__type movements__type--${type}">${
-//       i + 1
-//     } ${type}</div>
-//         <div class="movements__date">${displayDate}</div>
-//         <div class="movements__value">${formattedMov}</div>
-//       </div>
-//     `;
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+        <div class="movements__date">${displayDate}</div>
+        <div class="movements__value">${formattedMov}</div>
+      </div>
+    `;
 
-//     containerMovements.insertAdjacentHTML('afterbegin', html);
-//   });
-// };
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
